bc50f957d3a61a1598bac65971aa0346121a4c4c
Christophe <christophalpha@yahoo.com>
2020-10-23 20:23:36 +0200

    added listeners on keyboard: document.addEventListeners('keypress',()=>...) for +-*/ 0-9 = Enter; preventDefault on keypress of 'Enter'

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function sendNumberValue(number) {
   } else {
     // If current display value is 0, replace it, if not add number to display value
     const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = 
+    calculatorDisplay.textContent =
       (displayValue === '0')
-      ?
-      number
-      :
-      displayValue + number;
+        ?
+        number
+        :
+        displayValue + number;
   }
 }
 
@@ -43,6 +43,7 @@ const calculate = {
   '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
   '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
   '=': (firstNumber, secondNumber) => secondNumber,
+  'Enter': (firstNumber, secondNumber) => secondNumber,
   'Pow': (firstNumber, secondNumber) => (Math.pow(firstNumber, secondNumber)),
 };
 
@@ -51,41 +52,43 @@ function useFunction(funct) {
   awaitingNextValue = true;
   switch (funct) {
     case "Abs":
-        calculatorDisplay.textContent = Math.abs(x);
+      calculatorDisplay.textContent = Math.abs(x);
       break;
     case "SqRoot":
-        calculatorDisplay.textContent = Math.round(Math.sqrt(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.sqrt(x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "Exp":
-        calculatorDisplay.textContent = Math.round(Math.exp(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.exp(x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "Ln":
-        calculatorDisplay.textContent = Math.round(Math.log(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.log(x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "Sin":
-        calculatorDisplay.textContent = Math.round(Math.sin(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.sin(x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "Cos":
-        calculatorDisplay.textContent = Math.round(Math.cos(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.cos(x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "Tan":
-        calculatorDisplay.textContent = Math.round(Math.tan(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.tan(x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "Sign":
-        {calculatorDisplay.textContent = (-x);
-        keepOperating = true;}
+      {
+        calculatorDisplay.textContent = (-x);
+        keepOperating = true;
+      }
       break;
     case "Rand":
-        calculatorDisplay.textContent = Math.round(Math.random() * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(Math.random() * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "1/":
-        calculatorDisplay.textContent = Math.round(1/(x) * Math.pow(10,15))/ Math.pow(10,15);
+      calculatorDisplay.textContent = Math.round(1 / (x) * Math.pow(10, 15)) / Math.pow(10, 15);
       break;
     case "^2":
-        calculatorDisplay.textContent = Math.pow(x,2);
+      calculatorDisplay.textContent = Math.pow(x, 2);
       break;
     default:
-        calculatorDisplay.textContent = funct +"= Other Funct of ("+x+")";
+      calculatorDisplay.textContent = funct + "= Other Funct of (" + x + ")";
       break;
   }
 }
@@ -93,11 +96,11 @@ function useFunction(funct) {
 function useConst(constante) {
   awaitingNextValue = true;
   switch (constante) {
-    case "PI": 
-      calculatorDisplay.textContent = Math.PI     
+    case "PI":
+      calculatorDisplay.textContent = Math.PI
       break;
-    case "E": 
-      calculatorDisplay.textContent = Math.E     
+    case "E":
+      calculatorDisplay.textContent = Math.E
       break;
     default:
       break;
@@ -106,7 +109,6 @@ function useConst(constante) {
 
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
-  console.log("useOperator: operator =",operator,"; currentValue =",currentValue);
   // Prevent multiple operators
   if (operatorValue && awaitingNextValue && !keepOperating) {
     operatorValue = operator;
@@ -152,4 +154,70 @@ function resetAll() {
 }
 
 // Event Listener
-clearBtn.addEventListener('click', resetAll);
+clearBtn.addEventListener('click', () => resetAll());
+
+// Detect key press for valid characters and process
+//document.onkeypress = logKey; <=> document.addEventListener('keypress', logKey);
+document.addEventListener('keypress', logKey);
+function keyboardKeysAccepted(event) {
+  return (event.charCode === 0 || /[-+*\/0-9.\r=]/.test(String.fromCharCode(event.charCode)));
+}
+function logKey(e) {
+  //preventDefault to avoid 'Enter' to trigger also 'Click' on last selected field  
+  e.preventDefault();
+
+  /* console.log("Keyboard Key Pressed: code=", ` ${e.code}`, " key=", e.key, "; Check if accepted =>", keyboardKeysAccepted(e)); */
+
+  // Store key pressed data, defaults to undefined.
+  if (keyboardKeysAccepted(e)) {
+    // console.log('input key = \'',e.key, '\' is OK for processing');
+    switch (e.key) {
+      // Calculate '+ - * /' and '='
+      case (e.key.match(/[-+*\/=]/) || {}).input:
+        {
+          useOperator(e.key);
+          break;
+        }
+      // Enter
+      case 'Enter':
+        {
+          useOperator(e.key);
+          break;
+        }
+      /* 
+      // =
+      case '=':
+        {
+          useOperator(e.key);
+          break;
+        } 
+      */
+      // Only numbers.
+      //('|| {}' for avoidning .input on null error)
+      case (e.key.match(/\d/) || {}).input:
+        {
+          sendNumberValue(e.key);
+          break;
+        }
+      // Decimal 
+      case '.':
+        {
+          addDecimal(e.key);
+          break;
+        }
+      // Clear screen.
+      /* case 'Delete':
+        {
+          keyPressed = 'CA';
+          break;
+        } */
+      default:
+        console.log(e.key, "=> not in cases! type=", typeof e.key);
+        break;
+    }
+  } else {
+    // Log input for troubleshooting incorrect data.
+    console.log('Keyboard input key = \'', e.key, '\' is not supported for this calculator.\nPlease use numbers 0-9; operators + - * /; decimal point . ; Enter or =, or clic buttons displayed.');
+    alert('Keyboard key = \' ' + e.key + ' \' is not supported for this calculator.\nUse numbers 0-9; operators + - * /; decimal point . ; Enter or =\nOr clic buttons displayed.');
+  }
+};
